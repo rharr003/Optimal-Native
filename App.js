b7662419efa7ad4f4b548358f63b3c238985a531
Scott Harrington <robertscottharrington@gmail.com>
2023-11-21 05:47:32 -0500

    added loader while db seeds

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { AppState } from "react-native";
+import { AppState, ActivityIndicator, View, StyleSheet } from "react-native";
 import { init, wipeDatabase } from "./util/sqlite/db";
 import { useEffect } from "react";
 import store from "./util/redux/store";
@@ -9,6 +9,7 @@ import { useState } from "react";
 import * as Notifications from "expo-notifications";
 import MainTabNavigator from "./MainTabNavigator";
 import * as SplashScreen from "expo-splash-screen";
+import { ColorPalette } from "./ColorPalette";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => {
@@ -37,6 +38,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [appState, setAppState] = useState(AppState.currentState);
+  const [loading, setLoading] = useState(true);
   async function initNotifications() {
     const allowed = await allowsNotificationsAsync();
     if (!allowed) {
@@ -51,8 +53,9 @@ export default function App() {
     async function load() {
       // await wipeDatabase();
       await init();
-      initNotifications();
+      await initNotifications();
       await SplashScreen.hideAsync();
+      setLoading(false);
     }
     load();
   }, []);
@@ -74,9 +77,27 @@ export default function App() {
     };
   }, [appState]);
 
+  if (loading) {
+    return (
+      <View style={styles.backdrop}>
+        <ActivityIndicator color={ColorPalette.dark.gray500} size="large" />
+      </View>
+    );
+  }
+
   return (
     <Provider store={store}>
       <MainTabNavigator />
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  backdrop: {
+    backgroundColor: ColorPalette.dark.gray800,
+    flex: 1,
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+  },
+});
